00c046959175ea8bbedc067ef8303d0a44233991
Danh13320031 <danh13320031@gmail.com>
2026-04-21 21:24:13 +0700

    #feat: add functionality to change cart item quantity (product) in cart.

--- src/modules/cart/dto/update-cart.dto.ts
+++ src/modules/cart/dto/update-cart.dto.ts
@@ -1,1 +1,15 @@
-export class UpdateCartDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class UpdateCartItemQuantityDto {
+  @ApiProperty({
+    description: 'Số lượng mô hình',
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty({ message: 'Số lượng khóng được bỏ trống' })
+  @Type(() => Number)
+  @IsNumber({ maxDecimalPlaces: 0 }, { message: 'Số lượng phải là một số' })
+  quantity!: number;
+}
